f29a77827547bdc8b7c80180b80c111572392d99
VicenteRV <juan-vizz@hotmail.com>
2025-01-09 20:20:26 -0600

    Login - generarjwt - jwt en cookies terminado

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -32,18 +32,26 @@ const exceptoActual = async(correo,id)=>{
     }
 }
 
-
 const existeUsuarioDBdesactivado = async(id)=>{
     const existe = await Usuario.findOne({_id:id,estado:false});
     if(!existe){
         throw new Error(`El usuario con id: ${id} no existe o esta activo`)
     }
 }
+
+const validarCorreoUsuario = async(correo = '')=>{
+    const existeUsuario = await Usuario.findOne({correo});
+    if(!existeUsuario){
+        throw new Error(`El correo/contraseña son incorrectos - correo`)
+    }
+}
+
 module.exports = {
     usuarioExistente,
-   validacionRol,
-   existeUsuarioDB,
-   existeUsuarioActivo,
-   existeUsuarioDBdesactivado,
-   exceptoActual,
+    validacionRol,
+    existeUsuarioDB,
+    existeUsuarioActivo,
+    existeUsuarioDBdesactivado,
+    exceptoActual,
+    validarCorreoUsuario,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,16 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
 const { login } = require("../controllers/auth");
+const { validarCorreoUsuario } = require("../helpers/db-validators");
 
 const router = Router();
 
 router.post('/login',[
-
+    check('correo').notEmpty().withMessage('El correo es obligatorio')
+        .isEmail().withMessage('El correo no es válido'),
+    check('correo').custom(validarCorreoUsuario),
+    check('password').notEmpty().withMessage('La contraseña es obligatoria'),
+    validarCampos
 ],login);
 
 module.exports = router;
